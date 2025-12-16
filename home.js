7917852f66139de5956bd9bb430c565f3c4b72b7
altheaiyah <altheamishkaalvarez@gmail.com>
2025-12-17 02:31:03 +0800

    Add images, javascript, and initial structure of home page

---
+++ home.js
@@ -0,0 +1,29 @@
+var homeData = [
+  {
+    name: "Super Mario",
+    bgColor: "#C8102E",
+    imageSmall: "img/home_mario_sm.png",
+    imageMedium: "img/home_mario_md.png",
+    logo: "img/Logo_Mario_Series.svg",
+    link: "mario.html",
+    logoHeight: 150
+  },
+  {
+    name: "Legend of Zelda",
+    bgColor: "#2E7D32",
+    imageSmall: "img/home_zelda_sm.png",
+    imageMedium: "img/home_zelda_md.png",
+    logo: "img/Logo_Zelda.svg",
+    link: "zelda.html",
+    logoHeight: 150
+  },
+  {
+    name: "Animal Crossing",
+    bgColor: "#6EC6E8",
+    imageSmall: "img/home_ac_sm.png",
+    imageMedium: "img/home_ac_md.png",
+    logo: "img/Logo_Ac.svg",
+    link: "ac.html",
+    logoHeight: 200
+  }
+];
